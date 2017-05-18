987dd79d0346111cb2580df5b1098fa7174ed5a2
stevendecock <decocksteven79@gmail.com>
2017-05-18 22:15:55 +0200

    Initial commit

---
+++ src/main/java/be/decock/steven/cardbot/MainApplication.java
@@ -0,0 +1,57 @@
+package be.decock.steven.cardbot;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class MainApplication extends JFrame {
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainApplication frame = new MainApplication();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public MainApplication() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1280, 720);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        new MyThread().start();
+    }
+
+    VideoCap videoCap = new VideoCap();
+
+    public void paint(Graphics g){
+        g = contentPane.getGraphics();
+        g.drawImage(videoCap.getOneFrame(), 0, 0, this);
+    }
+
+    class MyThread extends Thread{
+        @Override
+        public void run() {
+            for (;;){
+                repaint();
+                try { Thread.sleep(30);
+                } catch (InterruptedException e) {    }
+            }
+        }
+    }
+}
